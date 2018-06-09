71563874230e32c0c08e5a96033d20e29abbf77e
tinny <tinnystudios@gmail.com>
2018-06-09 15:25:08 +1000

    - Cleaned up some files - Added FlowerCutExample scene. - Added Flower&Scissors Prefabs

--- UnityProject/Assets/BLINDED_AM_ME package/Scripts/ExampleUseof_MeshCut.cs
+++ UnityProject/Assets/BLINDED_AM_ME package/Scripts/ExampleUseof_MeshCut.cs
@@ -1,28 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using BLINDED_AM_ME;
 
 public class ExampleUseof_MeshCut : MonoBehaviour {
 
 	public Material capMaterial;
 
-	// Use this for initialization
-	void Start () {
-
-		
-	}
-	
 	void Update(){
 
 		if(Input.GetMouseButtonDown(0)){
+            //Create an empty raycast hit to assign later on
 			RaycastHit hit;
 
+            //If you hit an object in front of you (transform.forward) //Only returns 1 object.
 			if(Physics.Raycast(transform.position, transform.forward, out hit)){
-
+                //Create a victim object for any hit.
 				GameObject victim = hit.collider.gameObject;
+                //Create pieces that it will get cut into 
+				GameObject[] pieces = MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
 
-				GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+                //pieces[0] = left side
+                //pieces[1] = right side
 
-				if(!pieces[1].GetComponent<Rigidbody>())
+                //If Rigidbody does not exist on pieces[1] then Add a RIGIDBODY
+                if (!pieces[1].GetComponent<Rigidbody>())
 					pieces[1].AddComponent<Rigidbody>();
 
 				Destroy(pieces[1], 1);
@@ -30,6 +31,7 @@ public class ExampleUseof_MeshCut : MonoBehaviour {
 
 		}
 	}
+
 
 	void OnDrawGizmosSelected() {
 
